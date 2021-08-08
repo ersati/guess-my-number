fe8850190c95cc102458069b6c96a3e04e43be9c
ersati <piotrp92@gmail.com>
2021-08-08 20:53:47 +0100

    get all elements to manipulate

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 'use strict';
+ /* Elements */
+
+
+ const headline = document.querySelector('h1');
+ const reset = document.querySelector('.again');
+ const number = document.querySelector('.number');
+ const enterValue = document.querySelector('.guess');
+ const checkButton = document.querySelector('.check');
+ const message = document.querySelector('.message');
+ const score = document.querySelector('.score');
+ const highscore = document.querySelector('.highscore');
