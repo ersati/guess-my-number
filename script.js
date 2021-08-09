82457608256af5285fa5c7bcde4fb32dd5d5278e
ersati <piotrp92@gmail.com>
2021-08-09 01:56:37 +0100

    add function to check value of the number

--- script.js
+++ script.js
@@ -10,3 +10,20 @@
  const message = document.querySelector('.message');
  const score = document.querySelector('.score');
  const highscore = document.querySelector('.highscore');
+
+ const randomNumber = Math.floor((Math.random() * 20) +1);
+
+ number.textContent = randomNumber
+
+function changeValue(){
+if(enterValue.value > number){
+    message.textContent = 'more'
+} else if(enterValue.value < number){
+    message.textContent = 'less'
+}else if ( enterValue.value === number){
+    message.textContent = 'you won '
+}
+
+}
+
+ enterValue.addEventListener('change',changeValue);
