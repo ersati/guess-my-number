00449045a7c62df2acc49ab220a89396d4410fa2
ersati <piotrp92@gmail.com>
2021-09-02 18:28:51 +0100

    add validation and style to the app

--- script.js
+++ script.js
@@ -1,29 +1,50 @@
 'use strict';
- /* Elements */
+/* Elements */
 
 
- const headline = document.querySelector('h1');
- const reset = document.querySelector('.again');
- const number = document.querySelector('.number');
- const enterValue = document.querySelector('.guess');
- const checkButton = document.querySelector('.check');
- const message = document.querySelector('.message');
- const score = document.querySelector('.score');
- const highscore = document.querySelector('.highscore');
+const headline = document.querySelector('h1');
+const reset = document.querySelector('.again');
+const number = document.querySelector('.number');
+const enterValue = document.querySelector('.guess');
+const checkButton = document.querySelector('.check');
+const message = document.querySelector('.message');
+const score = document.querySelector('.score');
+const highscore = document.querySelector('.highscore');
 
- const randomNumber = Math.floor((Math.random() * 20) +1);
-
- number.textContent = randomNumber
-
-function changeValue(){
-if(enterValue.value > number){
-    message.textContent = 'more'
-} else if(enterValue.value < number){
-    message.textContent = 'less'
-}else if ( enterValue.value === number){
-    message.textContent = 'you won '
+const randomNumber = Math.floor((Math.random() * 20) + 1);
+let scoreNumber = 20
+number.textContent = randomNumber
+score.textContent = scoreNumber
+let highscoreNumber = 0;
+function changeValue() {
+    console.log(!enterValue.value, number, randomNumber)
+    if(!enterValue.value){
+        message.textContent = 'please put correct number inside the field'
+        return
+    }
+    if (scoreNumber !== 0) {
+        if (enterValue.value > randomNumber) {
+            message.textContent = 'more'
+            scoreNumber--
+            score.textContent = scoreNumber
+        } else if (enterValue.value < randomNumber) {
+            message.textContent = 'less'
+            scoreNumber--
+            score.textContent = scoreNumber
+        } else if (enterValue.value == randomNumber) {
+            message.textContent = 'you won '
+            document.body.style.backgroundColor = "green";
+            checkButton.disabled = true;
+            if(highscoreNumber < scoreNumber){
+                highscoreNumber = scoreNumber;
+                highscore.textContent = highscoreNumber;
+            }
+        }
+    } else {
+        message.textContent = 'you lost';
+        document.body.style.backgroundColor = "red";
+        checkButton.disabled = true;
+    }
 }
 
-}
-
- enterValue.addEventListener('change',changeValue);
+checkButton.addEventListener('click', changeValue);
