6cbd6982046ea7d34014e02c1f87d9fff164c11d
ersati <piotrp92@gmail.com>
2021-09-03 19:30:00 +0100

    create function to reset the game and fix tbugs

--- script.js
+++ script.js
@@ -11,11 +11,14 @@ const message = document.querySelector('.message');
 const score = document.querySelector('.score');
 const highscore = document.querySelector('.highscore');
 
-const randomNumber = Math.floor((Math.random() * 20) + 1);
+let randomNumber = generateNumber();
 let scoreNumber = 20
-number.textContent = randomNumber
 score.textContent = scoreNumber
 let highscoreNumber = 0;
+
+function generateNumber() {
+    return Math.floor((Math.random() * 20) + 1)
+}
 function changeValue() {
     console.log(!enterValue.value, number, randomNumber)
     if(!enterValue.value){
@@ -24,16 +27,17 @@ function changeValue() {
     }
     if (scoreNumber !== 0) {
         if (enterValue.value > randomNumber) {
-            message.textContent = 'more'
+            message.textContent = 'less'
             scoreNumber--
             score.textContent = scoreNumber
         } else if (enterValue.value < randomNumber) {
-            message.textContent = 'less'
+            message.textContent = 'more'
             scoreNumber--
             score.textContent = scoreNumber
         } else if (enterValue.value == randomNumber) {
             message.textContent = 'you won '
-            document.body.style.backgroundColor = "green";
+            document.body.style.backgroundColor ="green";
+            number.textContent = randomNumber;
             checkButton.disabled = true;
             if(highscoreNumber < scoreNumber){
                 highscoreNumber = scoreNumber;
@@ -47,4 +51,15 @@ function changeValue() {
     }
 }
 
+function resetGame() {
+    randomNumber = generateNumber()
+    number.textContent = '?';
+    score.textContent = 20;
+    scoreNumber = 20
+    document.body.style.backgroundColor = "#222";
+    message.textContent = 'Start guessing...';
+    checkButton.disabled = false;
+}
+
 checkButton.addEventListener('click', changeValue);
+reset.addEventListener('click', resetGame);
